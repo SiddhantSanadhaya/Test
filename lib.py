e682d5f99db1942e27954c0d3fa4a47da3adf2a1
Siddhant Sanadhaya <sid.sanadhaya@gmail.com>
2024-07-07 13:01:38 +0530

    substraction feature added

---
+++ lib.py
@@ -0,0 +1,9 @@
+class Calc:
+    def __init__(self,a,b) -> None:
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a+self.b 
+    def substract(self):
+        return self.a-self.b
+
